05d0b59c62bf97254eacda320966db08f509ac4a
samuelegastaldo <samuelegastaldo@Lab06_19.itseinaudi.local>
2016-01-23 08:37:40 +0100

    fatto nuovo scontrino

--- SpesaGrafica/src/gastaldogrando/SpesaGraficaTest.java
+++ SpesaGrafica/src/gastaldogrando/SpesaGraficaTest.java
@@ -7,10 +7,12 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class SpesaGraficaTest {
 
 	protected Shell shell;
+	private Text scontrino_text;
 
 	/**
 	 * Launch the application.
@@ -51,7 +53,8 @@ public class SpesaGraficaTest {
 		Button btnNuovoScontrino = new Button(shell, SWT.NONE);
 		btnNuovoScontrino.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(SelectionEvent e){
+				scontrino_text.setText("");
 			}
 		});
 		btnNuovoScontrino.setBounds(259, 21, 101, 25);
@@ -61,23 +64,16 @@ public class SpesaGraficaTest {
 		lblTesseraFedelt.setBounds(23, 31, 81, 15);
 		lblTesseraFedelt.setText("Tessera fedelt\u00E0:");
 		
-		Button btnNewButton = new Button(shell, SWT.NONE);
-		btnNewButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(110, 21, 28, 25);
-		btnNewButton.setText("SI");
+		Button btnSi = new Button(shell, SWT.CHECK);
+		btnSi.setBounds(116, 30, 36, 16);
+		btnSi.setText("Si");
 		
-		Button btnNewButton_1 = new Button(shell, SWT.NONE);
-		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-			}
-		});
-		btnNewButton_1.setBounds(144, 21, 28, 25);
-		btnNewButton_1.setText("NO");
+		Button btnNo = new Button(shell, SWT.CHECK);
+		btnNo.setBounds(158, 30, 36, 16);
+		btnNo.setText("No");
+		
+		scontrino_text = new Text(shell, SWT.BORDER);
+		scontrino_text.setBounds(259, 66, 128, 114);
 
 	}
 }
